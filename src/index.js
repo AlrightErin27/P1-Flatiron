137b3feeeb992d7f7a56bb0bab3e2e7f68f99973
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-07 11:29:48 -0800

    added thedogapi data locally

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //console.log("Reading loud and clear! 📻");
+  console.log("Reading loud and clear! 📻");
 
-  const imgRandom = document.querySelector("#imgRandom");
+  //   const imgRandom = document.querySelector("#imgRandom");
 
-  function getRandomImg() {
-    const dogAPI = "https://dog.ceo/api/breeds/image/random";
+  //   function getRandomImg() {
+  //     const dogAPI = "https://dog.ceo/api/breeds/image/random";
 
-    fetch(dogAPI)
-      .then((res) => res.json())
-      .then(function (json) {
-        console.log(json);
-        const imageUrl = json.message;
-        imgRandom.src = imageUrl;
-      })
-      .catch((err) => console.log("ERROR️‍🔥:", err));
-  }
-  getRandomImg();
+  //     fetch(dogAPI)
+  //       .then((res) => res.json())
+  //       .then(function (json) {
+  //         console.log(json);
+  //         const imageUrl = json.message;
+  //         imgRandom.src = imageUrl;
+  //       })
+  //       .catch((err) => console.log("ERROR️‍🔥:", err));
+  //   }
+  //   getRandomImg();
 });
