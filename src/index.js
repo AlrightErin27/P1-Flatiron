21788293fb713ff920039cf5fe42ca769527dc98
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-07 08:35:56 -0800

    added erins branch, and playing with fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Reading loud and clear! 📻");
+
+  //fetch from Dog API (https://dog.ceo/dog-api/?ref=apilist.fun)
+  const dogAPI = "https://dog.ceo/api/breeds/image/random";
+  fetch(dogAPI)
+    .then((res) => res.json())
+    .catch((err) => console.log("ERROR️‍🔥:", err));
+
+  function renderDogs(dogsArray) {
+    dogsArray.forEach(displayDog);
+  }
+  function displayDog(dog) {
+    let img = document.createElement("img");
+  }
 });
